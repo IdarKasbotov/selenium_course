c8c5cdd4236be8984ba4bb7b93859f52cb659070
Idar Kasbotov <idar.kasbotov@gmail.com>
2023-07-03 20:10:37 +0300

    add chapter_4

---
+++ chapter_4/tests/test_main_page.py
@@ -0,0 +1,12 @@
+"""
+https://stepik.org/lesson/199980/step/6?unit=174035
+"""
+from chapter_4.pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
